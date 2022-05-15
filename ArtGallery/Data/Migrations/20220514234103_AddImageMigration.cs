c4d14a86bd15560495beaa6dc8440fed1a907db6
richardhaynes <rahayn@yahoo.com>
2022-05-15 14:05:58 -0400

    adding images folder and migration

---
+++ ArtGallery/Data/Migrations/20220514234103_AddImageMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtGallery.Data.Migrations
+{
+    public partial class AddImageMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FilePath = table.Column<string>(type: "TEXT", nullable: false),
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
---
+++ ArtGallery/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtGallery.Models
+{
+	public class Image
+	{
+		[Key]
+		public int Id { get; set; }
+		public string? FilePath { get; set; }
+		[Required]
+		public Guid Guid { get; set; } = Guid.NewGuid();
+		[NotMapped,Required(ErrorMessage = "Image Must be Provided"), Display(Name ="File To Upload")]
+        public IFormFile? FormFile { get; set; }
+		public DateOnly CreatedDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+	}
+}
+
