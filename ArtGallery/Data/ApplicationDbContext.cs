6faee2232f14c124be0c4c3c5433cdd3f494b59f
richardhaynes <rahayn@yahoo.com>
2022-05-15 13:24:45 -0400

    Add Gallery Logic

--- ArtGallery/Data/ApplicationDbContext.cs
+++ ArtGallery/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ArtGallery.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ArtGallery.Data;
@@ -8,5 +9,16 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    public DbSet<Image> Images { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Image>(entity => {
+            entity.Property(e => e.Id);
+            entity.Property(e => e.Guid);
+            entity.Property(e => e.FilePath);
+            entity.Property(e => e.CreatedDate);
+        });
     }
 }
--- ArtGallery/Pages/Index.cshtml.cs
+++ ArtGallery/Pages/Index.cshtml.cs
@@ -1,19 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ArtGallery.Data;
+using ArtGallery.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtGallery.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    [BindProperty]
+    public Image ImageToUpload { get; set; }
+    public List<Image> Gallery { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext, IWebHostEnvironment webHostEnvironment)
     {
         _logger = logger;
+        _dbContext = dbContext;
+        _webHostEnvironment = webHostEnvironment;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet()
     {
+        Gallery = await _dbContext.Images.ToListAsync();
+        return Page();
+    }
 
+    public async Task<IActionResult> OnPost()
+    {
+        if(!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty,"Model In Invalid State");
+            return Page();
+        }
+        if(ImageToUpload.FormFile is null)
+        {
+            ModelState.AddModelError(string.Empty, "File Name not Found");
+            return Page();
+        }
+        try
+        {
+            string imageFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+            string filename = $"{ImageToUpload.Guid}_{ImageToUpload.FormFile.FileName}";
+            string filePath = Path.Combine(imageFolder, filename);
+            ImageToUpload.FilePath = filename;
+            if(!System.IO.File.Exists(filePath))
+            {
+                using (var stream = new FileStream(filePath, FileMode.OpenOrCreate))
+                {
+                    await ImageToUpload.FormFile.CopyToAsync(stream);
+                }
+                await _dbContext.Images.AddAsync(ImageToUpload);
+                await _dbContext.SaveChangesAsync();
+                Gallery = await _dbContext.Images.ToListAsync();
+                return Page();
+            }
+            ModelState.AddModelError(string.Empty, "File Path Exists");
+            return Page();
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"{ex}");
+            return RedirectToPage("Error");
+        }
     }
 }
